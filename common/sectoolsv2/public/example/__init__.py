72caf3e3fc1f4e39c26df83c1ac0568be77294bc
Michael Ni (nim) <quic_nim@quicinc.com>
2024-03-06 09:20:50 +0800

    Delete common/sectoolsv2 directory
    delete the directory
    
    Signed-off-by: Michael Ni (nim) <quic_nim@quicinc.com>

--- common/sectoolsv2/public/example/__init__.py
+++
@@ -1,5 +0,0 @@
-#  ===============================================================================
-#  Copyright (c) 2021 Qualcomm Technologies, Inc.
-#  All Rights Reserved.
-#  Confidential and Proprietary - Qualcomm Technologies, Inc.
-#  ===============================================================================
